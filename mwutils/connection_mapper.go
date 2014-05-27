d56b4bc4858d5c9d34fec1ea6c2d2eee559895b1
Pawel Chojnacki <pawel@chojnacki.ws>
2014-05-28 01:06:19 +0200

    add support for storing and retrieving connection object via req.Object

---
+++ mwutils/connection_mapper.go
@@ -0,0 +1,95 @@
+package mwutils
+
+import (
+	"net/http"
+	"sync"
+	"time"
+)
+
+// taken and simplified from gorilla/context
+// to be used in 3rd party handler to access Connection Object
+
+var (
+	mutex sync.RWMutex
+	data  = make(map[*http.Request]*Connection)
+	datat = make(map[*http.Request]int64)
+)
+
+func MapperSet(val *Connection) {
+	r := val.Request
+	mutex.Lock()
+	data[r] = val
+	datat[r] = time.Now().Unix()
+	mutex.Unlock()
+}
+
+// GetOk returns stored value and presence state like multi-value return of map access.
+func MapperGetOk(r *http.Request) (*Connection, bool) {
+	mutex.RLock()
+	value, ok := data[r]
+	mutex.RUnlock()
+	return value, ok
+}
+
+// Clear removes all values stored for a given request.
+//
+// This is usually called by a handler wrapper to clean up request
+// variables at the end of a request lifetime. See ClearHandler().
+func MapperClear(r *http.Request) {
+	mutex.Lock()
+	clear(r)
+	mutex.Unlock()
+}
+
+// clear is Clear without the lock.
+func clear(r *http.Request) {
+	delete(data, r)
+	delete(datat, r)
+}
+
+// Purge removes request data stored for longer than maxAge, in seconds.
+// It returns the amount of requests removed.
+//
+// If maxAge <= 0, all request data is removed.
+//
+// This is only used for sanity check: in case context cleaning was not
+// properly set some request data can be kept forever, consuming an increasing
+// amount of memory. In case this is detected, Purge() must be called
+// periodically until the problem is fixed.
+func MapperPurge(maxAge int) int {
+	mutex.Lock()
+	count := 0
+	if maxAge <= 0 {
+		count = len(data)
+		data = make(map[*http.Request]*Connection)
+		datat = make(map[*http.Request]int64)
+	} else {
+		min := time.Now().Unix() - int64(maxAge)
+		for r := range data {
+			if datat[r] < min {
+				clear(r)
+				count++
+			}
+		}
+	}
+	mutex.Unlock()
+	return count
+}
+
+// ClearHandler wraps an http.Handler and clears request values at the end
+// of a request lifetime.
+func ConnectionMapperFuncWrapper(f func(w http.ResponseWriter, r *http.Request)) func(con *Connection) {
+	return func(con *Connection) {
+		MapperSet(con)
+		defer MapperClear(con.Request)
+		f(con.ResponseWriter, con.Request)
+	}
+}
+
+func ConnectionMapperHandlerWrapper(h http.Handler) func(con *Connection) {
+	return func(con *Connection) {
+		MapperSet(con)
+		defer MapperClear(con.Request)
+		h.ServeHTTP(con.ResponseWriter, con.Request)
+	}
+}
